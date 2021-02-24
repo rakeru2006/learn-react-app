8dfe101d973183b3e5ee84586be9b8e74e1e594c
rakeru2006 <rakeru2006@gmail.com>
2021-02-23 18:00:56 -0600

    adding boostrap table

--- employapp/src/App.js
+++ employapp/src/App.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react'
 import { unstable_renderSubtreeIntoContainer } from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
 
 class App extends Component {
@@ -18,7 +23,8 @@ class App extends Component {
     .then((response) => {
       this.setState({
         items:response.results,
-        loading:true
+        loading:true,
+      
       })
     })
   }
@@ -34,13 +40,58 @@ class App extends Component {
       else{
 
       return(
+
+    
+
         <div className="container">
+        <Jumbotron fluid>
+        
+            <h1>Company Directory</h1>
+            <p>
+              Render images of employ 
+            </p>
+       
+        </Jumbotron>
+
+        <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td> item.name.first</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td colSpan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</Table>
+
+
 
         {items.map(item => (
-
+        
          
-          <img src={item.picture.medium } alt = {item.name.first}/>
+        <img src={item.picture.medium } alt = {item.name.first}/>
 
+
+        
         ))}
 
         </div>
@@ -51,30 +102,3 @@ class App extends Component {
 
 export default App;
 
-/*
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-*/
